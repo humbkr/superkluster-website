a5c4033872597573fae94743408a5f81f64f6014
Julien Galletta <humbkr@gmail.com>
2019-10-11 16:09:50 +0200

    Add logo.

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 
 export default () => (
   <Teaser>
-    <Title>superKluster</Title>
-    <Subtitle>coming soon...</Subtitle>
+    <Logo src="/static/images/logo.png" />
   </Teaser>
 )
 
@@ -16,15 +15,8 @@ const Teaser = styled.div`
   top: 50%;
   transform: translateY(-50%);
 `
-const Title = styled.h1`
-  font-size: 45px;
+const Logo = styled.img`
   margin-bottom: 20px;
-  color: ${({ theme }) => theme.colors.primary};
-  text-shadow: 0px 0px 9px #000;
-`
-const Subtitle = styled.h2`
-  font-size: 16px;
-  font-family: Arial;
-  color: ${({ theme }) => theme.colors.primary};
-  text-shadow: 0px 0px 9px #000;
+  width: 100%;
+  max-width: 600px;
 `
